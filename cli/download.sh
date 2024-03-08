2f0bdd6b555e98e634075d3a00e2368b47bc0be6
sameplacewei <sameplace@126.com>
2024-03-08 15:44:25 +0800

    download,sh

---
+++ cli/download.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# . ./cli/show.sh
+
+wget -O arcology/bin/arcology https://github.com/arcology-network/binary-releases/releases/download/v2.0.0/arcology
+wget -O ethereum/geth https://github.com/arcology-network/binary-releases/releases/download/v2.0.0/geth 
+wget -O op/bin/op-batcher https://github.com/arcology-network/binary-releases/releases/download/v2.0.0/op-batcher
+wget -O op/bin/op-node https://github.com/arcology-network/binary-releases/releases/download/v2.0.0/op-node
+wget -O op/bin/op-proposer https://github.com/arcology-network/binary-releases/releases/download/v2.0.0/op-proposer 
