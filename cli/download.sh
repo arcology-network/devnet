71b8e7c249abdf3160a2a16366e18afe0cbdb46c
sameplacewei <sameplace@126.com>
2024-05-25 22:14:52 +0800

    add monitor tools and version parameter

--- cli/download.sh
+++ cli/download.sh
@@ -1,8 +1,24 @@
 #!/usr/bin/env bash
 # . ./cli/show.sh
 
-wget -O arcology/bin/arcology https://github.com/arcology-network/binary-releases/releases/download/v2.0.0/arcology
-wget -O ethereum/geth https://github.com/arcology-network/binary-releases/releases/download/v2.0.0/geth 
-wget -O op/bin/op-batcher https://github.com/arcology-network/binary-releases/releases/download/v2.0.0/op-batcher
-wget -O op/bin/op-node https://github.com/arcology-network/binary-releases/releases/download/v2.0.0/op-node
-wget -O op/bin/op-proposer https://github.com/arcology-network/binary-releases/releases/download/v2.0.0/op-proposer 
+while getopts v: OPT; do
+ case ${OPT} in
+  v) version=${OPTARG}
+    ;;
+  \?)
+    printf "[Usage] `date '+%F %T'` -v <version>>\n" >&2
+    exit 1
+ esac
+done 
+
+if [ "${version}" == "" ]
+then
+  echo "please input current version ( -v X.X.X)"
+  exit 1
+fi
+
+wget -O arcology/bin/arcology https://github.com/arcology-network/binary-releases/releases/download/v${version}/arcology
+wget -O ethereum/geth https://github.com/arcology-network/binary-releases/releases/download/v${version}/geth 
+wget -O op/bin/op-batcher https://github.com/arcology-network/binary-releases/releases/download/v${version}/op-batcher
+wget -O op/bin/op-node https://github.com/arcology-network/binary-releases/releases/download/v${version}/op-node
+wget -O op/bin/op-proposer https://github.com/arcology-network/binary-releases/releases/download/v${version}/op-proposer 
